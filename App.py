a4c11d834d082726335034d54eb974825caeea95
patricmuema207-max <patricmuema207@gmail.com>
2026-05-25 11:37:04 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("AI Customer Review Sentiment Analyzer")
+
+@st.cache_resource
+def load_model():
+    return pipeline(
+        "text-classification",
+        model="patricmuema20/patric-sentiment-model"
+    )
+
+classifier = load_model()
+
+review = st.text_area("Enter customer review")
+
+if st.button("Analyze"):
+    if review.strip() != "":
+        result = classifier(review)
+        st.subheader("Prediction Result")
+        st.write(result)
+    else:
+        st.warning("Please enter a review.")
